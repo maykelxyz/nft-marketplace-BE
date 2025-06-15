13c83e40d99a1c92b3944cdbdada3c253da6f603
maykelxyz <mjtpediglorio@gmail.com>
2025-06-15 18:08:27 +0800

    [Feat] Created collections

--- src/controllers/CollectionController.go
+++ src/controllers/CollectionController.go
@@ -1,1 +1,17 @@
 package controllers
+
+import (
+	"nft-marketplace-be/src/config"
+	"nft-marketplace-be/src/services"
+)
+
+type CollectionController struct {
+	envConfig         config.ENVConfig
+	collectionService services.CollectionServiceInterface
+}
+
+func NewCollectionController(envConfig config.ENVConfig, collectionService services.CollectionServiceInterface) *CollectionController {
+	return &CollectionController{
+		envConfig: envConfig,
+	}
+}
--- src/services/collectionservice.go
+++ src/services/collectionservice.go
@@ -1,1 +1,38 @@
 package services
+
+import (
+	"nft-marketplace-be/src/config"
+	"nft-marketplace-be/src/models"
+)
+
+type CollectionServiceInterface interface {
+	Count(address string) (int, error)
+	ListFromAddress(address string) ([]models.Nft, error)
+}
+
+type CollectionService struct {
+	envConfig config.ENVConfig
+}
+
+func NewCollectionService(envConfig config.ENVConfig) CollectionServiceInterface {
+	return &CollectionService{
+		envConfig: envConfig,
+	}
+}
+
+func (s *CollectionService) Count(address string) (int, error) {
+	count, err := s.envConfig.DB.Model(&models.Nft{}).Where("collection_address = ?", address).Count()
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (s *CollectionService) ListFromAddress(address string) ([]models.Nft, error) {
+	var nfts []models.Nft
+	err := s.envConfig.DB.Model(&models.Nft{}).Where("collection_address = ?", address).Find(&nfts).Error
+	if err != nil {
+		return nil, err
+	}
+	return nfts, nil
+}
